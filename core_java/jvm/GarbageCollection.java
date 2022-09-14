6e46fc33ced15b4a65e9c7d2f2b5dc734eafa9fa
Pankaj Gaurav <pankajgaurav17@gmail.com>
2022-09-15 02:27:40 +0530

    Adding jvm related questions

---
+++ core_java/jvm/GarbageCollection.java
@@ -0,0 +1,10 @@
+package jvm;
+
+/**
+ *  Well Explained here :
+ *      https://www.oracle.com/webfolder/technetwork/tutorials/obe/java/gc01/index.html
+ */
+
+public class GarbageCollection {
+
+}
